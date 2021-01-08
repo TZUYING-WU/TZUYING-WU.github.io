6162640a1de610937399471eb583df31ca18005b
TZUYING-WU <71428465+TZUYING-WU@users.noreply.github.com>
2021-01-09 02:08:23 +0800

    Add files via upload

---
+++ demo/assets/js/add.js
@@ -0,0 +1,25 @@
+$(function() {
+    $(".plplus").click(function(e) {
+    e.preventDefault();
+    fieldName = $(this).attr("field");
+    var currentVal = parseInt($("input[name=" + fieldName + "]").val());
+    if (!isNaN(currentVal)) {
+    $("input[name=" + fieldName + "]").val(currentVal + 1);
+    } else 
+    {
+    $("input[name=" + fieldName + "]").val(0);
+    }
+    });
+    $(".plminus").click(function(e) 
+    {
+    e.preventDefault();
+    fieldName = $(this).attr("field");
+    var currentVal = parseInt($("input[name=" + fieldName + "]").val());
+    if (!isNaN(currentVal) && currentVal > 0) {
+    $("input[name=" + fieldName + "]").val(currentVal - 1);
+    } else 
+    {
+    $("input[name=" + fieldName + "]").val(0);
+    }
+    });
+    });
